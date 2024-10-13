3e50914a226b6491d905028659c6407fb7449725
Jiho Lee <ijiho5826@gmail.com>
2024-10-13 16:32:44 +0900

    EnemyFSM 생성 중
    컴퓨터 용량 없어 다시 다운로드

---
+++ Source/Fortress/Private/EnemyFSM.cpp
@@ -0,0 +1,79 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyFSM.h"
+
+// Sets default values for this component's properties
+UEnemyFSM::UEnemyFSM()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UEnemyFSM::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UEnemyFSM::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	FString LogMsg = UEnum::GetValueAsString(mState);
+	GEngine->AddOnScreenDebugMessage(0, 1, FColor::Cyan, LogMsg);
+	
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	switch (mState)
+	{
+	case EEnemyState::Idle:
+		IdleState();
+		break;
+	case EEnemyState::Move:
+		MoveState();
+		break;
+	case EEnemyState::Attack:
+		AttackState();
+		break;
+	case EEnemyState::Damage:
+		DamageState();
+		break;
+	case EEnemyState::Die:
+		DieState();
+		break;
+	}
+}
+
+void UEnemyFSM::IdleState()
+{
+
+}
+
+void UEnemyFSM::MoveState()
+{
+
+}
+
+void UEnemyFSM::AttackState()
+{
+
+}
+
+void UEnemyFSM::DamageState()
+{
+
+}
+
+void UEnemyFSM::DieState()
+{
+
+}
+
